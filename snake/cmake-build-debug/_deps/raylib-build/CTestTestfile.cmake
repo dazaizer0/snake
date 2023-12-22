c2607279398dc70ed1066523e942494542dafe7c
dazai <60112867+dazaizer0@users.noreply.github.com>
2023-12-22 13:35:27 +0100

    snake 2

---
+++ snake/cmake-build-debug/_deps/raylib-build/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: C:/Users/mydat/Documents/_active_c/snake/cmake-build-debug/_deps/raylib-src
+# Build directory: C:/Users/mydat/Documents/_active_c/snake/cmake-build-debug/_deps/raylib-build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("raylib")
