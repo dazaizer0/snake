9c7f482c728dab5a34d855e59833e1346661d34b
dazai <60112867+dazaizer0@users.noreply.github.com>
2023-12-22 13:20:29 +0100

    Delete cmake-build-debug directory

--- cmake-build-debug/_deps/raylib-build/raylib/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Users/mydat/Documents/_active_c/snake/cmake-build-debug/_deps/raylib-src/src
-# Build directory: C:/Users/mydat/Documents/_active_c/snake/cmake-build-debug/_deps/raylib-build/raylib
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("external/glfw")
